d9e8a0956338c27f50792eb58ed90e11b5fa2d67
Bewtaich <bewtaich@gmail.com>
2024-09-15 10:53:00 -0500

    fixed login/signup page display

--- client/src/components/Nav/nav.jsx
+++ client/src/components/Nav/nav.jsx
@@ -1,55 +1,90 @@
-import React from "react";
-import Auth from "../../utils/auth";
-import { Link, useNavigate } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Menu, Icon, Sidebar, SidebarPusher, Segment, Header, Image, MenuItem } from 'semantic-ui-react';
+import Auth from '../../utils/auth';
 
 function NavBar() {
-  const navigate = useNavigate(); 
+  const [visible, setVisible] = useState(false);
+  const navigate = useNavigate();
 
-  function showNavigation() {
-    return (
-      <Menu inverted fixed="top">
-        <Menu.Item name="home" onClick={() => navigate("/")}>
-          Home
-        </Menu.Item>
-        <Menu.Item name="about" onClick={() => alert("Click to earn money. Upgrade your zoo and become the richest zoo owner.")}>
-          About
-        </Menu.Item>
-        <Menu.Item name="leaderboard" onClick={() => alert('Leaderboard')}>
-          Leaderboard
-        </Menu.Item>
-        <Menu.Menu position="right">
-          {Auth.loggedIn() ? (
-            <>
-              <Menu.Item
-                name="logout"
-                onClick={() => {
-                  Auth.logout();
-                  navigate("/");  // Redirect to home after logout
-                }}
-              >
-                Logout
-              </Menu.Item>
-            </>
-          ) : (
-            <>
-              <Menu.Item name="signup" onClick={() => navigate("/signup")}>
-                Signup
-              </Menu.Item>
-              <Menu.Item name="login" onClick={() => navigate("/login")}>
-                Login
-              </Menu.Item>
-            </>
-          )}
-        </Menu.Menu>
-      </Menu>
-    );
-  }
+  const handleMenuClick = (path) => {
+    navigate(path);
+    setVisible(false); // Hide sidebar when a menu item is clicked
+  };
 
   return (
-    <header>
-      <nav>{showNavigation()}</nav>
-    </header>
+    <SidebarPusher>
+      {/* Sidebar for mobile */}
+      <Sidebar
+        as={Menu}
+        animation='overlay'
+        icon='labeled'
+        inverted
+        onHide={() => setVisible(false)}
+        vertical
+        visible={visible}
+        width='thin'
+      >
+        <MenuItem as='a' onClick={() => handleMenuClick("/")}>
+          <Icon name='home' />
+          Home
+        </MenuItem>
+        <MenuItem as='a' onClick={() => alert('Leaderboard')}>
+          <Icon name='trophy' />
+          Leaderboard
+        </MenuItem>
+        {Auth.loggedIn() ? (
+          <MenuItem as='a' onClick={() => Auth.logout()}>
+            <Icon name='sign-out' />
+            Logout
+          </MenuItem>
+        ) : (
+          <>
+            <MenuItem as='a' onClick={() => handleMenuClick("/signup")}>
+              <Icon name='signup' />
+              Signup
+            </MenuItem>
+            <MenuItem as='a' onClick={() => handleMenuClick("/login")}>
+              <Icon name='login' />
+              Login
+            </MenuItem>
+          </>
+        )}
+      </Sidebar>
+
+      <SidebarPusher>
+        <Segment basic>
+          <Menu inverted fixed='top'>
+            <Menu.Item onClick={() => setVisible(!visible)}>
+              <Icon name='sidebar' />
+            </Menu.Item>
+            <Menu.Item header>Zoo Tycoon</Menu.Item>
+            <Menu.Menu position='right'>
+              <Menu.Item as='a' onClick={() => handleMenuClick("/")}>
+                Home
+              </Menu.Item>
+              <Menu.Item as='a' onClick={() => alert('Leaderboard')}>
+                Leaderboard
+              </Menu.Item>
+              {Auth.loggedIn() ? (
+                <Menu.Item as='a' onClick={() => Auth.logout()}>
+                  Logout
+                </Menu.Item>
+              ) : (
+                <>
+                  <Menu.Item as='a' onClick={() => handleMenuClick("/signup")}>
+                    Signup
+                  </Menu.Item>
+                  <Menu.Item as='a' onClick={() => handleMenuClick("/login")}>
+                    Login
+                  </Menu.Item>
+                </>
+              )}
+            </Menu.Menu>
+          </Menu>
+        </Segment>
+      </SidebarPusher>
+    </SidebarPusher>
   );
 }
 
