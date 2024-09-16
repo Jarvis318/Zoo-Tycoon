ec20c6312866cbebad7fd780f5d2409e7c6f9ef7
Bewtaich <bewtaich@gmail.com>
2024-09-16 15:27:26 -0500

    upgrade tweak

--- client/src/components/Nav/nav.jsx
+++ client/src/components/Nav/nav.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Menu, Icon, Sidebar, SidebarPusher, Segment, MenuItem } from 'semantic-ui-react';
-import Auth from '/src/utils/auth';
-import BackgroundMusic from '../BackgroundMusic';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Menu,
+  Icon,
+  Sidebar,
+  SidebarPusher,
+  Segment,
+  MenuItem,
+} from "semantic-ui-react";
+import Auth from "/src/utils/auth";
+import BackgroundMusic from "../BackgroundMusic";
+import UpgradeItem from "./upgradeButton";
 
 function NavBar() {
   const [sidebarVisible, setSidebarVisible] = useState(false);
@@ -17,58 +25,58 @@ function NavBar() {
 
   const handleSoundToggle = () => {
     setSoundOn(!soundOn);
-    console.log(`Sound is ${soundOn ? 'Off' : 'On'}`);
+    console.log(`Sound is ${soundOn ? "Off" : "On"}`);
   };
 
   return (
     <>
-      <BackgroundMusic soundOn={soundOn} /> 
+      <BackgroundMusic soundOn={soundOn} />
       <SidebarPusher>
         {/* Sidebar*/}
         <Sidebar
           as={Menu}
-          animation='overlay'
-          icon='labeled'
+          animation="overlay"
+          icon="labeled"
           inverted
           onHide={() => setSidebarVisible(false)}
           vertical
           visible={sidebarVisible}
-          width='thin'
+          width="thin"
         >
-          <MenuItem as='a' onClick={() => handleMenuClick("/")}>
-            <Icon name='home' />
+          <MenuItem as="a" onClick={() => handleMenuClick("/")}>
+            <Icon name="home" />
             Home
           </MenuItem>
-          <MenuItem as='a' onClick={() => handleMenuClick('/leaderboard')}>
-            <Icon name='trophy' />
+          <MenuItem as="a" onClick={() => handleMenuClick("/leaderboard")}>
+            <Icon name="trophy" />
             Leaderboard
           </MenuItem>
           {Auth.loggedIn() ? (
             <>
-              <MenuItem as='a' onClick={() => Auth.logout()}>
-                <Icon name='sign-out' />
+              <MenuItem as="a" onClick={() => Auth.logout()}>
+                <Icon name="sign-out" />
                 Logout
               </MenuItem>
-              <MenuItem as='a' onClick={() => setUpgradesVisible(true)}>
-                <Icon name='settings' />
+              <MenuItem as="a" onClick={() => setUpgradesVisible(true)}>
+                <Icon name="settings" />
                 Upgrades
               </MenuItem>
             </>
           ) : (
             <>
-              <MenuItem as='a' onClick={() => handleMenuClick("/signup")}>
-                <Icon name='signup' />
+              <MenuItem as="a" onClick={() => handleMenuClick("/signup")}>
+                <Icon name="signup" />
                 Signup
               </MenuItem>
-              <MenuItem as='a' onClick={() => handleMenuClick("/login")}>
-                <Icon name='login' />
+              <MenuItem as="a" onClick={() => handleMenuClick("/login")}>
+                <Icon name="login" />
                 Login
               </MenuItem>
             </>
           )}
-          <MenuItem as='a' onClick={handleSoundToggle}>
-            <Icon name={soundOn ? 'volume up' : 'volume off'} />
-            {soundOn ? 'Sound On' : 'Sound Off'}
+          <MenuItem as="a" onClick={handleSoundToggle}>
+            <Icon name={soundOn ? "volume up" : "volume off"} />
+            {soundOn ? "Sound On" : "Sound Off"}
           </MenuItem>
         </Sidebar>
 
@@ -76,49 +84,48 @@ function NavBar() {
         {Auth.loggedIn() && (
           <Sidebar
             as={Menu}
-            animation='overlay'
-            icon='labeled'
+            animation="overlay"
+            icon="labeled"
             inverted
             onHide={() => setUpgradesVisible(false)}
             vertical
             visible={upgradesVisible}
-            direction='right'
-            width='thin'
+            direction="right"
+            width="thin"
+            className="upgrade-sidebar"
+            style={{ maxHeight: "100vh", overflowY: "auto" }}
           >
-            <MenuItem as='a' onClick={() => alert('Upgrade 1')}>
-              <Icon name='cogs' />
-              Upgrade 1
-            </MenuItem>
-            <MenuItem as='a' onClick={() => alert('Upgrade 2')}>
-              <Icon name='cogs' />
-              Upgrade 2
-            </MenuItem>
-            <MenuItem as='a' onClick={() => alert('Upgrade 3')}>
-              <Icon name='cogs' />
-              Upgrade 3
-            </MenuItem>
+            <UpgradeItem
+              name="Click Power"
+              level={clickPower.level}
+              price={clickPower.price}
+              canAfford={clickPower.canAfford}
+              iconName="hand pointer"
+            />
           </Sidebar>
         )}
 
         <SidebarPusher>
           <Segment basic>
-            <Menu inverted fixed='top'>
+            <Menu inverted fixed="top">
               <Menu.Item onClick={() => setSidebarVisible(!sidebarVisible)}>
-                <Icon name='sidebar' />
+                <Icon name="sidebar" />
               </Menu.Item>
-              <Menu.Item header style={{ flex: 1, textAlign: 'center' }}>
+              <Menu.Item header style={{ flex: 1, textAlign: "center" }}>
                 Zoo Tycoon
               </Menu.Item>
-              <Menu.Menu position='right'>
+              <Menu.Menu position="right">
                 {!Auth.loggedIn() && (
                   <Menu.Item onClick={() => handleMenuClick("/login")}>
-                    <Icon name='login' />
+                    <Icon name="login" />
                     Login
                   </Menu.Item>
                 )}
                 {Auth.loggedIn() && (
-                  <Menu.Item onClick={() => setUpgradesVisible(!upgradesVisible)}>
-                    <Icon name='settings' />
+                  <Menu.Item
+                    onClick={() => setUpgradesVisible(!upgradesVisible)}
+                  >
+                    <Icon name="settings" />
                     Upgrades
                   </Menu.Item>
                 )}
